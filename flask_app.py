7ab49cb9d2a77aefa81f595984063a9779df6118
Ramasamy2002 <112847391+Ramasamy2002@users.noreply.github.com>
2023-11-02 11:16:55 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,13 @@
 import os
 import pandas as pd
 from flask import Flask, render_template, request
-from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
 from tensorflow.keras.models import load_model
 import numpy as np
+from googleapiclient.discovery import build
+
+# Replace with your YouTube Data API key
+YOUTUBE_API_KEY = "AIzaSyAskP8XGk-kOuiSWowfEl3EsokF378K-vw"
 
 app = Flask(__name__)
 
@@ -19,32 +22,63 @@ with open('vectorizer_vocabulary.pkl', 'rb') as vocab_file:
 # Define sentiment labels
 sentiment_labels = ["Negative", "Neutral", "Positive"]
 
+# Function to fetch comments from a YouTube video using the YouTube API
+def fetch_youtube_comments(video_url, max_comments=5):
+    video_id = video_url.split('v=')[-1]
+
+    # Initialize the YouTube API client
+    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+
+    # Fetch the video comments
+    results = youtube.commentThreads().list(
+        part='snippet',
+        videoId=video_id,
+        textFormat='plainText',
+        maxResults=max_comments
+    ).execute()
+
+    comments = []
+    for item in results['items']:
+        comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+        comments.append(comment)
+
+    return comments
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict_sentiment():
-    user_input = request.form.get('comments')
+    user_input = request.form.get('video_url')
 
-    # Tokenize and vectorize the input comments
-    new_comments_vectorized = vectorizer.transform([user_input]).toarray()
+    # Fetch comments from the video
+    fetched_comments = fetch_youtube_comments(user_input, max_comments=5)
+    
+    # Make predictions for each comment
+    comment_sentiments = []
+    for comment in fetched_comments:
+        # Tokenize and vectorize the input comment
+        comment_vectorized = vectorizer.transform([comment]).toarray()
+        if comment_vectorized.shape[1] > 148:
+            comment_vectorized = comment_vectorized[:, :148]
+        elif comment_vectorized.shape[1] < 148:
+            padding = 148 - comment_vectorized.shape[1]
+            comment_vectorized = np.pad(comment_vectorized, ((0, 0), (0, padding)), 'constant')
+        
+        # Make predictions with your CNN model
+        predictions = model.predict(comment_vectorized)
+        
+        # Interpret predictions
+        predicted_label = sentiment_labels[np.argmax(predictions)]
+        
+        comment_sentiments.append((comment, predicted_label))
 
-    # Ensure that the input data has the expected shape (None, 145)
-    if new_comments_vectorized.shape[1] > 145:
-        new_comments_vectorized = new_comments_vectorized[:, :145]
-    elif new_comments_vectorized.shape[1] < 145:
-        # You can pad with zeros if needed
-        padding = 145 - new_comments_vectorized.shape[1]
-        new_comments_vectorized = np.pad(new_comments_vectorized, ((0, 0), (0, padding)), 'constant')
+    # Determine the overall sentiment based on majority sentiment
+    sentiment_counts = [sentiment for _, sentiment in comment_sentiments]
+    overall_sentiment = max(set(sentiment_counts), key=sentiment_counts.count)
 
-    # Make predictions with your CNN model
-    predictions = model.predict(new_comments_vectorized)
-
-    # Interpret predictions
-    predicted_label = sentiment_labels[np.argmax(predictions)]
-
-    return render_template('result.html', sentiment=predicted_label)
+    return render_template('result.html', sentiment=overall_sentiment, comment_sentiments=comment_sentiments)
 
 if __name__ == '__main__':
     app.run(debug=True)
